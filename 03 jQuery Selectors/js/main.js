a94527cac3b606d5617aeefcbce4f0a280d6c1fc
Mahmud <mdmahmudul413@gmail.com>
2023-07-11 12:45:12 +0600

    all set

--- 03 jQuery Selectors/js/main.js
+++ 03 jQuery Selectors/js/main.js
@@ -1,102 +1,121 @@
 $(document).ready(function() {
 
     // select button by id
-    // $('#btn').click(function() {
-    //     $("h1").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("h1").hide();
+        });
 
     // select all tags from dom using *
-    // $('#btn').click(function() {
-    //     $("*").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("*").hide();
+        });
 
 
     // to hide item by itself
-    // $('#btn').click(function() {
-    //     $("#btn").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("#btn").hide();
+        });
 
 
     // to hide item by itself using "this"
-    // $('#btn').click(function() {
-    //     $(this).hide();
-    // });
+
+        $('#btn').click(function() {
+            $(this).hide();
+        });
 
     // to hide item by itself using (:)
-    // $('#btn').click(function() {
-    //     $(":button").hide();
-    // });
+
+        $('#btn').click(function() {
+            $(":button").hide();
+        });
 
 
     // to hide h1 tag by itself using this
-    // $('.moon').click(function() {
-    //     $(this).hide();
-    // });
+
+        $('.moon').click(function() {
+            $(this).hide();
+        });
 
     // to select a specific p tag from multiple p tags and hide it by button 
-    // $('#btn').click(function() {
-    //     $("p.lorem").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("p.lorem").hide();
+        });
 
 
     //select by pseudo selectors
-    // first child 
-    // $('#btn').click(function() {
-    //     $("p:first").hide();
-    // });
 
-    // last child
-    // $('#btn').click(function() {
-    //     $("p:last").hide();
-    // });
+        // first child 
+
+            $('#btn').click(function() {
+                $("p:first").hide();
+            });
+
+        // last child
+
+            $('#btn').click(function() {
+                $("p:last").hide();
+            });
 
 
     // to hide the first child of list 1
-    // $('#btn').click(function() {
-    //     $("li:first").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("li:first").hide();
+        });
 
     // to hide the first child of list 1 and list 2 both
-    // $('#btn').click(function() {
-    //     $("li:first-child").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("li:first-child").hide();
+        });
 
 
-    // to hide the first child of list 1 and list 2 both
-    // $('#btn').click(function() {
-    //     $("li:last-child").hide();
-    // });
+    // to hide the last child of list 1 and list 2 both
+
+        $('#btn').click(function() {
+            $("li:last-child").hide();
+        });
 
     // to hide the nth child of list 1 and list 2 both
-    // $('#btn').click(function() {
-    //     $("li:nth-child(2)").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("li:nth-child(2)").hide();
+        });
 
 
-    // to hide a tag 
-    // $('#btn').click(function() {
-    //     $("a").hide();
-    // });
+    // to hide a tags
+
+        $('#btn').click(function() {
+            $("a").hide();
+        });
 
     // to hide 'a' tag another way 
-    // $('#btn').click(function() {
-    //     $("[href]").hide();
-    // });
+
+        $('#btn').click(function() {
+            $("[href]").hide();
+        });
 
     // to hide a link using it's attribute  
-    // $('#btn').click(function() {
-    //     $('[target="_blank"]').hide();
-    // });
+
+        $('#btn').click(function() {
+            $('[target="_blank"]').hide();
+        });
 
     // another way using backticks
-    // $('#btn').click(function() {
-    //     $(`[target="_blank"]`).hide();
-    // });
+
+        $('#btn').click(function() {
+            $(`[target="_blank"]`).hide();
+        });
 
     // another way
-    // $('#btn').click(function() {
-    //     $('a[target="_blank"]').hide();
-    // });
+
+        $('#btn').click(function() {
+            $('a[target="_blank"]').hide();
+        });
 
 
 });
